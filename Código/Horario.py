77e8a4d6b63754876c71ccb6f6884269e4388d0d
Felipe Derkian <felipekian@yahoo.com.br>
2020-04-24 23:31:05 -0400

    add imagens markdown

---
+++ Código/Horario.py
@@ -0,0 +1,30 @@
+
+while(True):
+    print("-----------------------------------------------\n")
+
+    hora = int(input("HORA: "))
+    minuto = int(input("MINUTO: "))
+
+    print()
+
+    if minuto == 0:
+        print("{} O`clock".format(hora))
+    elif minuto <= 30:
+        print("ITS ", end="")
+        if minuto == 30:
+            print("HALF ", end="")
+        elif minuto == 15:
+            print("QUARTER ", end="")
+        else:
+            print("{} ".format(minuto), end="")
+        print("PAST {}".format(hora), end="")
+    else:
+        print("ITS ", end="")
+        if minuto == 45:
+            print("QUARTER ", end="")
+        else:
+            print("{} ".format(60 - minuto), end="")
+        print("TO {}".format(hora+1), end="")
+
+    print()
+
